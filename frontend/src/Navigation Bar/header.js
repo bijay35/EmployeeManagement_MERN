26990781e754d4e1609e9cd98cd11864e0946cae
bijay35 <bijay.koirala53@gmail.com>
2023-04-07 14:00:08 -0400

    table design change

--- frontend/src/Navigation Bar/header.js
+++ frontend/src/Navigation Bar/header.js
@@ -1,28 +1,50 @@
-import { Link } from "react-router-dom"
-import  '.././App.scss'
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { MDBBtn, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import '.././App.scss'
+
+
 export const NavBar = () => {
-  const handleLout = ()=>{
+  const [isEmployeePage, setIsEmployeePage] = useState(false);
+  const location = useLocation();
+
+  const handleLout = () => {
     localStorage.removeItem("token");
   }
-return (
+
+  useEffect(() => {
+    setIsEmployeePage(location.pathname === '/employee');
+  }, [location]);
+
+  return (
     <div className="navbarCustom">
-      <button className="Logout" onClick={handleLout}><Link className="btn btn-light btn-outline-primary LogoutLink" to="/">logout <span><i className="fa-sharp fa-solid fa-right-from-bracket"  style={{ fontSize: "17px" }}></i></span></Link></button>
-       <nav className="navbar  navbar-expand-sm navbar-dark bg-light text-uppercase " style={{backgroundImage:"radial-gradient( circle 654px at 0.6% 48%,  rgba(12,170,255,1) 0%, rgba(151,255,129,1) 99.3% )"}}>
-        <div className="container-fluid " >
+      <MDBRow>
+        <MDBCol>
+          <Link to="/"><MDBBtn color='danger' onClick={handleLout} className="float-end">logout</MDBBtn></Link>
+        </MDBCol>
+
+      </MDBRow>
+      <MDBRow>
+        <nav className="navbar  navbar-expand-sm navbar-dark bg-light text-uppercase " style={{ backgroundImage: "radial-gradient( circle 654px at 0.6% 48%,  rgba(12,170,255,1) 0%, rgba(151,255,129,1) 99.3% )" }}>
+          <div className="container-fluid " >
             <ul className="navbar-nav ms-5 mt-2">
               <li className="nav-item">
-                
+
               </li>
               <li className="nav-item">
                 <Link className="btn btn-light btn-outline-primary" to="/department">Department</Link>
               </li>
               <li className="nav-item">
                 <Link className="btn btn-light btn-outline-primary" to="/employee">Employee</Link>
-               </li>
+              </li>
             </ul>
           </div>
-      </nav>
-        <h1 style={{textAlign:"center"}}>Employee Management System</h1>
+        </nav>
+      </MDBRow>
+
+      <h1 style={{ textAlign: "center" }}>
+        {isEmployeePage ? "Employee Details" : "Department Details"}
+      </h1>
     </div>
-)
+  )
 }
